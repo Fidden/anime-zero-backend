291fa5055e5eba815107dc0585b1ac7a9e646ee3
storm <sahalisa17@gmail.com>
2022-03-11 18:33:13 +0300

    Add files for fast deployment

---
+++ permissions.sh
@@ -0,0 +1,6 @@
+sudo chown -R $USER:www-data storage
+sudo chown -R $USER:www-data bootstrap/cache
+chmod -R 775 storage
+chmod -R 775 bootstrap/cache
+chmod -R 775 storage bootstrap/cache
+ chmod 777 -R storage bootstrap/cache
