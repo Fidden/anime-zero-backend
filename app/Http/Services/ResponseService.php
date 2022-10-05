37d89275e9b8e78591411f93a9be73f9d1872974
storm <sahalisa17@gmail.com>
2022-10-05 22:44:50 +0300

    Add response service

---
+++ app/Http/Services/ResponseService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Services;
+
+class ResponseService
+{
+    public static function error(string $message, int $code, mixed $payload = []): \Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory
+    {
+        return response([
+            'error' => [
+                'code' => $code,
+                'message' => $message,
+                'payload' => $payload,
+            ]
+        ], $code);
+    }
+
+    public static function success(mixed $data, int $code = 200): \Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory
+    {
+        return response([
+            'data' => $data,
+        ], $code);
+    }
+
+    public static function created(): \Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory
+    {
+        return response(null, 201);
+    }
+
+    public static function noContent(): \Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory
+    {
+        return response(null, 204);
+    }
+}
