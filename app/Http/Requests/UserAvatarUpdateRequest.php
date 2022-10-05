b9facf307995edfef6542d88a97c4f746cc975dd
storm <sahalisa17@gmail.com>
2022-10-05 22:51:13 +0300

    Refactored request code

--- app/Http/Requests/UserAvatarUpdateRequest.php
+++ app/Http/Requests/UserAvatarUpdateRequest.php
@@ -9,22 +9,7 @@ use Illuminate\Foundation\Http\FormRequest;
  */
 class UserAvatarUpdateRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
+    public function rules(): array
     {
         return [
             'image' => 'required|string',
--- app/Http/Requests/UserUpdateRequest.php
+++ app/Http/Requests/UserUpdateRequest.php
@@ -7,21 +7,6 @@ use Illuminate\Validation\Rule;
 
 class UserUpdateRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         return [
