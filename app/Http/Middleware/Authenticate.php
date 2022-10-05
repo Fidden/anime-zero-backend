633537b4b164f7ceaf278f54f9bf357d8e77c496
storm <sahalisa17@gmail.com>
2022-10-06 02:40:19 +0300

    Fix auth middleware return type error

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,7 +12,7 @@ class Authenticate extends Middleware
      * @param \Illuminate\Http\Request $request
      * @return string|null
      */
-    protected function redirectTo($request): ?string
+    protected function redirectTo($request)
     {
         if (!$request->expectsJson()) {
             session()->put('message', 'auth-required');
