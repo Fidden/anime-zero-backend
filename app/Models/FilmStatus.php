d75392b8995b5cca0b127dafe9d89ad9c51ece83
storm <sahalisa17@gmail.com>
2022-10-06 15:37:34 +0300

    Fixed film status

--- app/Models/FilmStatus.php
+++ app/Models/FilmStatus.php
@@ -4,13 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class FilmStatus extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'name',
-        'value',
+        'film_id',
+        'status_id',
     ];
+
+    public function status(): HasOne
+    {
+        return $this->hasOne(Status::class);
+    }
 }
--- database/migrations/2022_02_22_103317_create_statuses_table.php
+++ database/migrations/2022_02_22_103317_create_statuses_table.php
@@ -1,18 +1,17 @@
 <?php
 
-use App\Models\Film;
-use App\Models\Status;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     public function up()
     {
-        Schema::create('film_statuses', function (Blueprint $table) {
+        Schema::create('statuses', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Film::class);
-            $table->foreignIdFor(Status::class);
+            $table->string('name');
+            $table->enum('value', ['ongoing', 'released', 'announced', 'unknown']);
             $table->timestamps();
         });
     }
